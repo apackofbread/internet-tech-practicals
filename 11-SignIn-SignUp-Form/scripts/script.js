f344f2ef5cba39dccee7b4b1f78f163e0e6ccff5
Purnima <purnimak2001@gmail.com>
2021-11-24 17:39:46 +0530

    Delete 11-SignIn-SignUp-Form directory

--- 11-SignIn-SignUp-Form/scripts/script.js
+++
@@ -1,9 +0,0 @@
-//implement form buttons
-const signInBtn = document.getElementById("sign-in-btn");
-let usernames, passwords;
-signInBtn.addEventListener("click", function (e) {
-  e.preventDefault();
-});
-
-const signUpBtn = document.getElementById("sign-up-btn");
-signUpBtn.addEventListener("click", function () {});
